267c0d8f6f1305b14256ee913d3b316ac967bcd6
JESUS <jesus.quispe17@unmsm.edu.pe>
2021-09-06 01:36:45 -0500

    Commit #03: Avance Módulo Cliente

---
+++ database/migrations/2021_09_06_035707_create_dishes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('category',50);
+            $table->string('name',100);
+            $table->text('description')->nullable();
+            $table->float('price_normal', 4, 2);
+            $table->float('price_familiar', 4, 2)->nullable();
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}

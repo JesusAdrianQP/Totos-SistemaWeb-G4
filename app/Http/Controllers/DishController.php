13d3b0b04c1ad421a0125ffce28a7f8b402a1a80
Urpi de la Cruz <urpi.delacruz@unmsm.edu.pe>
2021-09-06 07:11:26 -0500

    Commit #05: Desarrollo del modulo cliente

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -11,48 +11,97 @@ class DishController extends Controller
     public function index(){
         $dishes = Dish::all();
         return view('custom.menu', compact('dishes'));
-        // $query=DB::table('products')->get();
-        // return view('products.index',['products'=>$query]);
     }
 
-    public function create(){
-        return view('dish.create'); //Vista del formulario de registro
+
+
+    // public function create(){
+    //     return view('dish.create'); //Vista del formulario de registro
+    // }
+
+    // public function store(Request $request){
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'number' => 'required',
+    //         'email' => 'required',
+    //         'subject' => 'required',
+    //         'description' => 'required'
+    //     ]);
+
+    //     $dishes = new Dish();
+    //     $dishes->title = $request->title;
+    //     $dishes->country = $request->country;
+    //     $dishes->price = $request->price;
+    //     $dishes->file = Storage::url($request->file('file')->store('public/images'));
+
+    //     $dishes->save();
+
+    //     return redirect()->route('products.index');
+    // }
+
+    // public function edit($id){
+    //     $products = Product::find($id);
+    //     return view('products.edit', compact('products'));
+    // }
+
+    // public function update(Request $request, $id){
+    //     $products = Product::find($id);
+    //     $products->update($request->all());
+    //     return redirect()->route('products.index');
+    // }
+
+    // public function destroy($id){
+    //     $products = Product::find($id);
+    //     $products->delete();
+    //     return redirect()->route('products.index');
+    // }
+    //-----------------LISTAR POR CATEGORIA---------------------
+    public function listOffers(Request $request){
+        
+        $dishes = Dish::where('category','like', 'ofertas')
+                        ->orderBy('id', 'DESC')
+                        ->simplePaginate(9);
+
+         return view('custom.menu', compact('dishes'));
+        
+    }
+    public function listPizzas(Request $request){
+        
+        $dishes = Dish::where('category','like', 'pizzas')
+                        ->orderBy('id', 'DESC')
+                        ->simplePaginate(9);
+
+         return view('custom.menu', compact('dishes'));
+        
     }
 
-    public function store(Request $request){
-        $request->validate([
-            'name' => 'required',
-            'number' => 'required',
-            'email' => 'required',
-            'subject' => 'required',
-            'description' => 'required'
-        ]);
+    public function listPastas(Request $request){
+        
+        $dishes = Dish::where('category','like', 'pastas')
+                        ->orderBy('id', 'DESC')
+                        ->simplePaginate(9);
 
-        $dishes = new Dish();
-        $dishes->title = $request->title;
-        $dishes->country = $request->country;
-        $dishes->price = $request->price;
-        $dishes->file = Storage::url($request->file('file')->store('public/images'));
-
-        $dishes->save();
-
-        return redirect()->route('products.index');
+         return view('custom.menu', compact('dishes'));
+        
     }
 
-    public function edit($id){
-        $products = Product::find($id);
-        return view('products.edit', compact('products'));
+    public function listDrinks(Request $request){
+        
+        $dishes = Dish::where('category','like', 'bebidas')
+                        ->orderBy('id', 'DESC')
+                        ->simplePaginate(9);
+
+         return view('custom.menu', compact('dishes'));
+        
     }
 
-    public function update(Request $request, $id){
-        $products = Product::find($id);
-        $products->update($request->all());
-        return redirect()->route('products.index');
-    }
+    public function listExtra(Request $request){
+        
+        $dishes = Dish::where('category','like', 'adicionales')
+                        ->orderBy('id', 'DESC')
+                        ->simplePaginate(9);
 
-    public function destroy($id){
-        $products = Product::find($id);
-        $products->delete();
-        return redirect()->route('products.index');
+         return view('custom.menu', compact('dishes'));
+        
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,18 @@ module.exports = {
         '800': '#999999', 
         '900': '#7d7d7d'
       },
+      'cuarto': {
+        '50': '#fffbf2', 
+        '100': '#fff6e6', 
+        '200': '#ffe9bf', 
+        '300': '#ffdc99', 
+        '400': '#ffc14d', 
+        '500': '#ffa700', 
+        '600': '#e69600', 
+        '700': '#bf7d00', 
+        '800': '#996400', 
+        '900': '#7d5200'
+    },
 
       transparent: 'transparent',
       current: 'currentColor',
